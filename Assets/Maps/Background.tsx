8f84100afe740b2191136048eecdb52bff824d6d
ToniLlovera <160241512+ToniLlovera@users.noreply.github.com>
2024-10-28 10:17:08 +0100

    Platform Game - Alpha_Src

---
+++ Assets/Maps/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Background" tilewidth="32" tileheight="32" tilecount="10120" columns="184">
+ <image source="Background.png" width="5906" height="1772"/>
+</tileset>
