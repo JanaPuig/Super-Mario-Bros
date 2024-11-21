a4e42779fb0369266653ca7ab945125e90f8ae07
J4an4 <160216332+J4an4@users.noreply.github.com>
2024-11-21 15:02:44 +0100

    Update Item

--- src/Item.cpp
+++ src/Item.cpp
@@ -23,8 +23,7 @@ bool Item::Start() {
 
 	//initilize textures
 	texture = Engine::GetInstance().textures.get()->Load(parameters.attribute("texture").as_string());
-	position.setX(parameters.attribute("x").as_int());
-	position.setY(parameters.attribute("y").as_int());
+
 	texW = parameters.attribute("w").as_int();
 	texH = parameters.attribute("h").as_int();
 
